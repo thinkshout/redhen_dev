715987594e1ab1747dd572d6dfa36e0a29083354
Lev Tsypin <lev@levelos.com>
2012-02-14 10:56:49 -0800

    Adding --no-gitinfofile flag to drush make.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 echo 'Drupal root is being deleted.'
 sudo rm -rf drupal
 echo 'Downloading project files.'
-drush make --working-copy ams.make drupal
+drush make --working-copy --no-gitinfofile ams.make drupal
 echo 'Installing Redhen.'
 cd drupal
 drush si --db-url=mysql://$1:$2@localhost/$3 -y
