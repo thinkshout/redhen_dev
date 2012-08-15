6825bfcc0b280153b17d65000e7c9bcdf422953c
Lev Tsypin <lev@thinkshout.com>
2012-08-15 12:28:37 -0700

    moving install profile to root.

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo 'Drupal root is being deleted.'
-sudo rm -rf drupal
-echo 'Downloading project files.'
-drush make --working-copy --no-gitinfofile ams.make drupal
-echo 'Installing Redhen.'
-cd drupal
-drush si ams_dev --db-url=mysql://$1:$2@localhost/$3 -y
